f9c0bc7b686fca9137dc389cc760fb4265d0315d
AnyClaw <l.kolryser@yandex.ru>
2026-09-21 12:30:57 +0300

    Initial commit

---
+++ src/test/java/ru/moskalenko/hr_platform/HrPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.moskalenko.hr_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
